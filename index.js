081403b682098b0a5deedb3929ace5a4460b9323
Matt Brouwer <notjonbonjovi@gmail.com>
2023-04-09 01:18:26 -0400

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(numberOfBlocks){
+    if (numberOfBlocks < 42){
+    return 42 - numberOfBlocks}
+    return numberOfBlocks - 42;
+
+}
+let numberOfBlocks = 1;
+
+function distanceFromHqInFeet(numberOfBlocks){
+  return  distanceFromHqInBlocks(numberOfBlocks) * 264
+}
+
+function distanceTravelledInFeet(firstBlock, lastBlock){
+    if (firstBlock > lastBlock) {
+        return (firstBlock - lastBlock) * 264;
+    }
+    return (lastBlock - firstBlock) * 264;
+}
+
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end) * 264);
+  }
+  
+  function calculatesFarePrice(start, end) {
+    const distanceTravelled = distanceTravelledInFeet(start, end); 
+    if (distanceTravelled < 400) {
+      return 0;
+    } else if (distanceTravelled >= 400 && distanceTravelled <= 2000) {
+      return ((distanceTravelled -400) * .02);
+    }
+    else if ((distanceTravelled >= 2000) && (distanceTravelled <= 2500)) {
+      return 25;
+    }
+    else {
+      return 'cannot travel that far';
+    }
+  }
